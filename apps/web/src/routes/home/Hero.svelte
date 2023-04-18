9bfc2464aa4009ab84f11a9dd60a6a7ccaf04625
juanmesa2097 <juanmesa2097@gmail.com>
2023-04-17 21:24:55 -0500

    feat(web): create initial implementation of hero component - #0

--- apps/web/src/routes/home/Hero.svelte
+++ apps/web/src/routes/home/Hero.svelte
@@ -1,21 +1,41 @@
 <script lang="ts">
+    import { CarouselTransition } from 'flowbite-svelte';
+
+    import carousel_2 from '$lib/assets/images/TMP_backdrop_0.jpg';
+    import carousel_3 from '$lib/assets/images/TMP_backdrop_1.jpg';
     import carousel_0 from '$lib/assets/images/TMP_carousel_0.jpg';
     import carousel_1 from '$lib/assets/images/TMP_carousel_1.jpg';
 
-    const slides = [
+    const images = [
+        {
+            id: 0,
+            name: 'Lorem',
+            imgurl: carousel_0,
+        },
         {
             id: 1,
-            width: 400,
-            height: 300,
-            src: carousel_0,
+            name: 'Ipsum',
+            imgurl: carousel_1,
         },
         {
             id: 2,
-            width: 400,
-            height: 300,
-            src: carousel_1,
+            name: 'Ipsum',
+            imgurl: carousel_2,
+        },
+        {
+            id: 3,
+            name: 'Ipsum',
+            imgurl: carousel_3,
         },
     ];
 </script>
 
-<div class="flex" />
+<CarouselTransition
+    {images}
+    showCaptions={false}
+    showThumbs={true}
+    showIndicators={true}
+    duration={6000}
+    transitionType="fade"
+    transitionParams={{ delay: 0, duration: 300 }}
+/>
