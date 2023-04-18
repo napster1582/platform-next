5e0f290a221e1291d9ea16b9bcb0f70ab73e99ae
juanmesa2097 <juanmesa2097@gmail.com>
2023-04-17 21:33:05 -0500

    feat(web): create menu base component - #0

--- apps/web/src/lib/components/menu/Menu.svelte
+++ apps/web/src/lib/components/menu/Menu.svelte
@@ -1,1 +1,27 @@
-<p>Menu</p>
+<script>
+    import { Navbar, NavHamburger, NavLi, NavUl } from 'flowbite-svelte';
+</script>
+
+<Navbar
+    let:hidden
+    let:toggle
+    color="form"
+    class="my-12"
+>
+    <div class="flex items-center justify-end">
+        Menú
+
+        <NavHamburger on:click={toggle} />
+    </div>
+
+    <NavUl
+        {hidden}
+        class="order-1"
+    >
+        <NavLi href="/">SEN</NavLi>
+
+        <NavLi href="/">SGDOC</NavLi>
+
+        <NavLi href="/">InnovARC</NavLi>
+    </NavUl>
+</Navbar>
--- apps/web/src/routes/home/+page.svelte
+++ apps/web/src/routes/home/+page.svelte
@@ -21,30 +21,6 @@
             porro expedita inventore, quisquam aliquam eos autem eius beatae ut animi, fugiat nemo
             quidem quod in voluptates. Aperiam natus maxime quis.
         </section>
-
-        <section>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta molestiae voluptatibus
-            quibusdam error perspiciatis ex, quam voluptatum delectus placeat expedita, quisquam
-            reprehenderit enim saepe! Illo laboriosam eos debitis, natus provident incidunt ipsum
-            magni ducimus iure itaque tenetur cupiditate tempore corporis ratione dolor saepe id
-            adipisci placeat numquam, possimus nisi? Cumque aliquam amet veritatis numquam ratione
-            sed dignissimos magni officia ut et eos ea praesentium voluptate fugiat, doloremque quae
-            accusamus minus ipsam laboriosam itaque. Magnam ipsum eligendi unde perferendis possimus
-            porro expedita inventore, quisquam aliquam eos autem eius beatae ut animi, fugiat nemo
-            quidem quod in voluptates. Aperiam natus maxime quis.
-        </section>
-
-        <section>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta molestiae voluptatibus
-            quibusdam error perspiciatis ex, quam voluptatum delectus placeat expedita, quisquam
-            reprehenderit enim saepe! Illo laboriosam eos debitis, natus provident incidunt ipsum
-            magni ducimus iure itaque tenetur cupiditate tempore corporis ratione dolor saepe id
-            adipisci placeat numquam, possimus nisi? Cumque aliquam amet veritatis numquam ratione
-            sed dignissimos magni officia ut et eos ea praesentium voluptate fugiat, doloremque quae
-            accusamus minus ipsam laboriosam itaque. Magnam ipsum eligendi unde perferendis possimus
-            porro expedita inventore, quisquam aliquam eos autem eius beatae ut animi, fugiat nemo
-            quidem quod in voluptates. Aperiam natus maxime quis.
-        </section>
     </main>
 
     <SocialNetworks />
