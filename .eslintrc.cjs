7df9a0622ea6e2cbd1fe34095932b234503bdf39
juanmesa2097 <juanmesa2097@gmail.com>
2023-05-15 07:31:23 -0500

    refactor: adjust general stuff - #7

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,12 @@
 module.exports = {
     root: true,
     parser: '@typescript-eslint/parser',
-    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'prettier',
+    ],
     plugins: ['svelte3', '@typescript-eslint', 'prettier'],
     ignorePatterns: ['*.cjs'],
     overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
