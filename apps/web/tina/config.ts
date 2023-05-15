3d08b137d9937e77c7a17eec9eec9f88cdfe37b7
juanmesa2097 <juanmesa2097@gmail.com>
2023-05-15 07:50:17 -0500

    chore(web): configure tina cloud - #0

--- apps/web/tina/config.ts
+++ apps/web/tina/config.ts
@@ -6,8 +6,8 @@ const branch = process.env.HEAD || 'main';
 
 export default defineConfig({
     branch,
-    clientId: null, // Get this from tina.io
-    token: null, // Get this from tina.io
+    clientId: '3196ac3d-3bb2-407c-964d-fcd082d52723', // Get this from tina.io
+    token: 'aca6de7ac422bfdd84574fd1b5e789727ba8bc3a', // Get this from tina.io
     build: {
         outputFolder: 'admin',
         publicFolder: 'static',
