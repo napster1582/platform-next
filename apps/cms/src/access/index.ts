4ebac6aeb6f747c467c6f27189c716b3fefe01ab
juanmesa2097 <juanmesa2097@gmail.com>
2023-06-28 05:44:26 -0500

    release: bump version to 0.1.18-alpha

--- apps/cms/src/access/index.ts
+++ apps/cms/src/access/index.ts
@@ -1,15 +1,33 @@
 import { Access } from 'payload/config';
 import { User } from 'payload/dist/auth';
 
-export const isAdmin: Access = ({ req: { user } }): boolean =>
-    checkRole(user, {
+export const isAdmin: Access = ({ req: { user } }): boolean => {
+    console.log(
+        'isAdmin',
+        user,
+        checkRole(user, {
+            roles: ['admin'],
+        }),
+    );
+
+    return checkRole(user, {
         roles: ['admin'],
     });
+};
 
-export const isUser: Access = ({ req: { user } }): boolean =>
-    checkRole(user, {
+export const isUser: Access = ({ req: { user } }): boolean => {
+    console.log(
+        'isUser',
+        user,
+        checkRole(user, {
+            roles: ['user'],
+        }),
+    );
+
+    return checkRole(user, {
         roles: ['user'],
     });
+};
 
 const checkRole = (user: User, options: { roles: string[] }): boolean =>
     options.roles.includes(user?.role as string);
