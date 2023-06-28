0524a2d0e4118bd0da656e4fc2b8a0a6d84a4feb
juanmesa2097 <juanmesa2097@gmail.com>
2023-06-28 10:17:05 -0500

    release: bump version to 0.1.25-alpha

---
+++ packages/jinen-helpers/src/objects/is-empty.ts
@@ -0,0 +1,7 @@
+export function isEmpty<ObjectType extends object | null | undefined>(obj: ObjectType): boolean {
+    if (typeof obj !== 'object') {
+        return true;
+    }
+
+    return obj === undefined || obj === null || Object.entries(obj).length === 0;
+}
