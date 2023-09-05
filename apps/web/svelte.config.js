5bdfa422511955927e56ac19660aa5d18d8b986a
napster1582@hotmail.com <diego.sanchez@talentui.co>
2023-09-05 09:57:42 -0500

    fix: changed adapster vercel

--- apps/web/svelte.config.js
+++ apps/web/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 import { dirname, join } from 'path';
 
@@ -13,10 +13,9 @@ const config = {
 	}),
 
 	kit: {
-		adapter: adapter({
-			precompress: true,
-			polyfill: false,
-		}),
+		adapter: adapter(
+			
+		),
 
 		csp: {
 			directives: {
